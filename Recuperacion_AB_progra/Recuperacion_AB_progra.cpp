06909ea9a8abc94de5fb47028d604c62494dd711
danieldelarosavalero <danieldelarosavalero@gmail.com>
2025-02-17 11:28:54 +0100

    Agregar archivos de proyecto.

---
+++ Recuperacion_AB_progra/Recuperacion_AB_progra.cpp
@@ -0,0 +1,12 @@
+﻿// Recuperacion_AB_progra.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "Recuperacion_AB_progra.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
